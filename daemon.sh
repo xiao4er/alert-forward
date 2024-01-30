c83ad57a43600710abbea643216ee8eec1143c29
fuxin.li <fuxin.li@transwarp.io>
2024-01-30 12:34:24 +0800

    fix daemon bug

--- daemon.sh
+++ daemon.sh
@@ -5,14 +5,14 @@
 # 定义一个函数，用于执行所有操作
 set -x
 while true; do
-
+    cd /transwarp/alert-forward || (echo cannot cd gitrepo && exit 1)
     # 执行 git pull 来更新代码
     echo "Updating project code..."
-    git pull origin main
+    sudo git pull origin main
 
     # 执行 upgrade.sh 脚本
     echo "Executing upgrade.sh script..."
-    ./auto-upgrade.sh
+    sudo ./auto-upgrade.sh
     sleep 7200
 
 done
